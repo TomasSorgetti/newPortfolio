2d4134d277defda5f158b56749412d384b854876
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2024-01-10 21:36:48 -0300

    fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        pinkColor: "#FE0072",
+        pinkColor2: "#E80ECD",
+        footerColor: "#ffffff54",
+      },
+    },
+  },
+  plugins: [],
+};
